25ab3315e00e08685fb6ea409c0b6f7de5c515c5
Muhammed Javis <muhammedjavis@gmail.com>
2023-10-31 14:12:08 +0530

    initialized project states responding to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSideBar from "./components/ProjectsSideBar";
 
 function App() {
+  const[projectsState,setProjectsState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+  })
+
+  function handleStartAddProject(){
+    setProjectsState((prevProjectsState)=>{
+     return { ...prevProjectsState,selectedProjectId:null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject/>
+  } else if (projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar/>
-     <NoProjectSelected/>
+      <ProjectsSideBar onStartAddProject={handleStartAddProject}/>
+     {content}
     </main>
   );
 }
